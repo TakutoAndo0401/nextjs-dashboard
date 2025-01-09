ba6b26bc4a25b4440f426229eb2972b942c29241
TakutoAndo0401 <terminus.rita@gmail.com>
2025-01-09 11:53:19 +0900

    feat: 「Chapter 5」までお勉強

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+/**
+ * ダッシュボードページ固有のレイアウト
+ * 「/dashboard」配下にしかここで定義したUIは適用されない
+ */
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
