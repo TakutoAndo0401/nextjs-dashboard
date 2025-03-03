3c0462daa85752f375c42e9122e3f4848be8fea3
TakutoAndo0401 <terminus.rita@gmail.com>
2025-03-03 15:14:07 +0900

    feat: feat: Chapter 11

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 /**
  * ルートレイアウト
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        {children}
+        <NuqsAdapter>{children}</NuqsAdapter>
 
         {/* Vercel Speed Insights Quickstart */}
         <SpeedInsights />
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,29 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { parseAsNumberLiteral, parseAsString, useQueryState } from "nuqs";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter } from "next/navigation";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+type Props = {
+  placeholder: string;
+};
+
+export default function Search({ placeholder }: Props) {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [params, setParams] = useQueryState("query");
+
+  function handleSearch(term: string) {
+    if (term) {
+      setParams(term);
+    } else {
+      setParams(null);
+    }
+
+    replace(`${pathname}?${params}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +32,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={params || ""}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
